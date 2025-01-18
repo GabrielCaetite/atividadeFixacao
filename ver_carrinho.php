39164584ab47b5cb73cdaeb69a210452db8efa11
GabrielCaetite <gabrielcaitite12@gmail.com>
2025-01-18 12:26:03 -0300

    implementando titulo e tabela

--- ver_carrinho.php
+++ ver_carrinho.php
@@ -3,12 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Visualizando carrinho</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
     <h2 class="titulo">Carrinho</h2>
+    <div class="carrinho">
+        <table >
+            <tr>
+                <th></td>
+                <th></td>
+                <th></td>
+            </tr>
+        </table>
+    </div>
 
 </body>
 </html>
