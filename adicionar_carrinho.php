9fefc767e690c8f48a860099aa8b92bd9a814350
GabrielCaetite <gabrielcaitite12@gmail.com>
2025-01-18 12:23:21 -0300

    validando adicao de itens ao carrinho

--- adicionar_carrinho.php
+++ adicionar_carrinho.php
@@ -1,43 +1,60 @@
 <?php
     session_start();
-    if(isset($_GET['adicionar'])){
-        echo '<script>alert("vai se fuder");</script>';
-    }
-    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Selecionando produtos</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    
     <h2 class="titulo">Podutos</h2>
     <div class="lista-produtos">
         <?php
-            $itens = array(['imagem'=>'bermuda.jpg', 'nome'=>'Bermuda', 'preco'=>'120,00'],
-            ['imagem'=>'tenis.jpg', 'nome'=>'Tênis', 'preco'=>'330,00'],
-            ['imagem'=>'relogio.webp', 'nome'=>'Relógio', 'preco'=>'240,00']);
+            $itens = array(['imagem'=>'bermuda.jpg', 'nome'=>'Bermuda Trekking', 'preco'=>'120,00'],
+            ['imagem'=>'tenis.jpg', 'nome'=>'Tênis Fila', 'preco'=>'330,00'],
+            ['imagem'=>'relogio.webp', 'nome'=>'Relógio Olevs', 'preco'=>'240,00']);
             
             foreach($itens as $key => $value){
         ?>
         
         <div class="produto">
-            <img src="<?php echo 'imagens/' . $value['imagem'] ?>" alt="Bermuda">
+            <img src="<?php echo 'imagens/' . $value['imagem'] ?>" alt="<?php echo $value['nome'] ?>">
             <p><?php echo $value['nome'] ?></p>
             <p><?php echo "R$ ".$value['preco'] ?></p>
             <a href="?adicionar=<?php echo $key ?>">Adicionar ao carrinho</a>
         </div>
 
         <?php
-            
             }
         ?>
         <br>
-        <a href="ver_carrinho.php">Visualizar Carrinho</a>
     </div>
+    <div class="acessar-carrinho">
+        <a href="ver_carrinho.php" >Carrinho</a>
+    </div>
+
+    <?php
+        if(isset($_GET['adicionar'])){
+            //se existe 'adicionar', o item sera adicionado ao carrinho
+            $idProduto = $_GET['adicionar'];
+            if(isset($itens[$idProduto])){
+                //se idProduto existe, a sessao associada tambem
+                if(isset($_SESSION[$idProduto])){
+                    $_SESSION[$idProduto]['quantidade']++;
+                }else{
+                    $_SESSION[$idProduto] = array('quantidade'=>'1', 'nome'=>$itens[$idProduto]['nome'],
+                    'preco'=>$itens[$idProduto]['preco']);
+                }
+                echo '<script>alert("O Item selecionando foi adicionado ao carrinho!");</script>';
+            }else{
+                echo '<script>alert("O Item não existe no carrinho!");</script>';
+            }
+        }
+    ?>
 
 </body>
 </html>
